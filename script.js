df18c8b2f886c852cc6262c2ea8bf603dceb6f26
Asim Das <asim71190@gmail.com>
2023-11-03 12:01:19 +0530

    Added No field error

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const passwordBox =document.getElementById("passbox");
+const passwordBox = document.getElementById("passbox");
 const copyBtn = document.getElementById("copy");
 const regenerateBtn = document.getElementById("regenerate");
 const rangeBox = document.getElementById("range-box");
@@ -7,53 +7,67 @@ const lowerBtn = document.getElementById("lower");
 const upperBtn = document.getElementById("upper");
 const numberBtn = document.getElementById("number");
 const symbolBtn = document.getElementById("symbol");
+const note = document.querySelector(".error-field");
+const close = document.querySelector(".error-field .note button")
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = upperCase.toLocaleLowerCase();
 const number = "1234567890";
 const symbol = "~!@#$%^&*()_+=-[]{}\\|/?><";
 
-function createPassword(){
+function createPassword() {
     let password = "";
     let allChar = "";
-    if(lowerBtn.checked){
-        allChar+=lowerCase;
+    if (lowerBtn.checked) {
+        allChar += lowerCase;
     }
-    if(upperBtn.checked){
-        allChar+=upperCase;
+    if (upperBtn.checked) {
+        allChar += upperCase;
     }
-    if(numberBtn.checked){
-        allChar+=number;
+    if (numberBtn.checked) {
+        allChar += number;
     }
-    if(symbolBtn.checked){
-        allChar+=symbol;
+    if (symbolBtn.checked) {
+        allChar += symbol;
     }
     if (upperBtn.checked == false && lowerBtn.checked == false && numberBtn.checked == false && symbolBtn.checked == false) {
         console.log("Check all the box");
+        note.style.display = "block"
+        setTimeout(() => {
+            note.style.display = "none"
+
+        },3500)
     }
-    else{
-        for(i = 1; i <=range.value; i++){
+    else {
+        for (i = 1; i <= range.value; i++) {
             password += allChar[Math.floor(Math.random() * allChar.length)];
         }
         passwordBox.value = password;
     }
 }
-range.addEventListener("input",()=>{
+close.addEventListener("click",()=>{
+    note.style.display = "none"
+})
+
+
+range.addEventListener("input", () => {
     rangeBox.value = range.value;
     createPassword();
 })
-rangeBox.addEventListener("input",()=>{
+rangeBox.addEventListener("input", () => {
     let value = Number.parseInt(rangeBox.value);
-    if(value >= 4 && value <= 50){
-        range.value  = value;
+    if (value >= 4 && value <= 50) {
+        range.value = value;
         createPassword();
     }
-    else{
+    else {
         let rangeError = document.getElementById("range-error");
         rangeError.style.display = "block"
-        setTimeout(()=>{
-        rangeError.style.display = "none"
-        },1500)
+        setTimeout(() => {
+            rangeError.style.display = "none"
+        }, 1500)
     }
 })
+
+regenerateBtn.addEventListener("click",createPassword)
 createPassword();
